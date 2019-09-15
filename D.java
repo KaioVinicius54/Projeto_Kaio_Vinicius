fe93fdfd0142abe2d5cd105c84d6a947a4583e53
KaioVinicius54 <kaio.silva@estudante.ufla.br>
2019-09-15 12:10:20 -0300

    -Inserção dos métodos MB3() e MC3() nas classes B e C, respectivamente. -Criação da classe D.

---
+++ D.java
@@ -0,0 +1,25 @@
+class D{
+    private float D1;
+    private String D2;
+
+    public D(float D1, String D2 ){
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+
+    public float getD1(){
+        return D1;
+    }
+
+    public String getD2(){
+        return D2;
+    }
+
+    public void MD1(){
+        System.out.println("MD1");
+    }
+
+    public void MD2(){
+        System.out.println("MD2");
+    }
+}
